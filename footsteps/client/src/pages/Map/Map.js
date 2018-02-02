41202d483afd3eee1f66a8aa6350016fb2373d84
Vishvesh Dave <vishvesh01@yahoo.com>
2018-02-01 21:24:11 -0500

    GOOGLE MAPS WORKING!!!

--- footsteps/client/src/pages/Map/Map.js
+++ footsteps/client/src/pages/Map/Map.js
@@ -1,20 +1,92 @@
 import React, { Component } from "react";
 import {Container } from "../../components/Grid";
+import "./Map.css";
+const google = window.google;
 
 class Map extends Component {
-	state = {
-		adventure: [],
-		location: "",
-		checkpoints: ""
-	};
+
+	constructor(props){
+		super(props);
+		this.state = {
+			adventure: [],
+			location: "",
+			checkpoints: "",
+			x: document.getElementById("demo"),
+		};
+	}
+
+	getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
+		} else {
+			// this.state.x.innerHTML = "Geolocation is not supported by this browser.";
+			this.setState({x: document.getElementById("demo").innerHTML("Geolocation is not supported by this browser.")})
+		}
+	}
+
+	componentDidMount(){
+		// window.map = new window.google.maps.Map(document.getElementById('map'), {
+		// 	center: { lat: 33.753746, lng: -84.386330 },
+		// 	zoom: 10,
+		// 	mapTypeId: 'roadmap',
+		// 	zoomControl: true
+		// });
+		//
+		// let marker = new google.maps.Marker({
+		// 	position: { lat: 33.753746, lng: -84.386330 },
+		// 	map: window.map
+		// });
+
+		this.getLocation();
+	}
+
+	// window.map
+
+	showPosition(position) {
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		var latlon = new window.google.maps.LatLng(lat, lon);
+		var mapholder = document.getElementById('map');
+
+		var myOptions = {
+			center:latlon,zoom:14,
+			mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+			mapTypeControl:false,
+			navigationControlOptions:{style:window.google.maps.NavigationControlStyle.SMALL}
+		}
+
+		var map = new window.google.maps.Map(document.getElementById("map"), myOptions);
+		var marker = new window.google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+	}
+
+	showError(error) {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+				// this.state.x.innerHTML = "User denied the request for Geolocation.";
+				this.setState({x: document.getElementById("demo").innerHTML("User denied the request for Geolocation.")});
+				break;
+			case error.POSITION_UNAVAILABLE:
+				// this.state.x.innerHTML = "Location information is unavailable.";
+				this.setState({x: document.getElementById("demo").innerHTML("Location information is unavailable.")});
+				break;
+			case error.TIMEOUT:
+				// this.state.x.innerHTML = "The request to get user location timed out.";
+				this.setState({x: document.getElementById("demo").innerHTML("The request to get user location timed out.")});
+				break;
+			case error.UNKNOWN_ERROR:
+				// this.state.x.innerHTML = "An unknown error occurred.";
+				this.setState({x: document.getElementById("demo").innerHTML("User denied the request for Geolocation.")});
+				break;
+		}
+	}
 
 	render() {
 		return (
 			<Container fluid>
 				<div className="row">
 					<div>
-						{/*<button onClick="getLocation()">Find Me</button>*/}
-						{/*<div id="mapholder"> </div>*/}
+						<button onClick={this.getLocation}>Find Me</button>
+						<div id="map"> </div>
 					</div>
 				</div>
 			</Container>
